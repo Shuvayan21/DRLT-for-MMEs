e9257af51f82847737d92a7e135b9fc72a796034
Shuvayan21 <banerjeeshuvayan21@gmail.com>
2026-01-20 20:01:45 +0530

    Update Distance_Decoder.m

--- Distance_Decoder.m
+++ Distance_Decoder.m
@@ -1,13 +1,15 @@
 function x_hat=Distance_Decoder(A, A_tilde, x, p, q)
+    o_vec = ones(50,p);
+    A = [A ; o_vec]
     [m,n]=size(A);
-    M_c=(A+1)/2;
-    M_s=(A_tilde+1)/2;
+    M_c=A;
+    M_s=A_tilde;
     y = any(M_s(:, x==1), 2);  % vector of test results
-
+    
     % 5. Distance decoder
     e = round((1 - p) * q * m);  % max number of flips per column tolerated
     x_hat = zeros(n, 1);
-
+    
     for i = 1:n
         c_i = M_c(:, i);
         % Count number of test positions where c_i == 1 but y == 0
@@ -16,5 +18,6 @@ function x_hat=Distance_Decoder(A, A_tilde, x, p, q)
             x_hat(i) = 1;
         end
     end
+
 
 end
