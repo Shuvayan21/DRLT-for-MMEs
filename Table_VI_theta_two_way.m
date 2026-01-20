c29a02f88eba504b5ad2c620af107df6f277d98b
Shuvayan21 <banerjeeshuvayan21@gmail.com>
2026-01-20 20:05:07 +0530

    Update Table_VI_theta_two_way.m

--- Table_VI_theta_two_way.m
+++ Table_VI_theta_two_way.m
@@ -15,15 +15,16 @@ Spec_DistD=zeros(size(actic_prob,2),1);
 for l=1:size(actic_prob,2)
     theta=actic_prob(l);
     %creating the data
-    [A,A_tilde,beta,delta,sigma]=data_create_vetterli_two_way(n,p,f_sig,f_sp,alpha,theta);
+    [B,B_tilde,A,A_tilde,beta,delta,sigma]=data_create_vetterli_two_way(n,p,f_sig,f_sp,alpha,theta);
 %% vetterli application
 x=abs(beta)>0;
-    [ Sens_beta, Spec_beta] = results_Sens_Spec_RRMSE_vetterli_two_way(A,A_tilde,x,theta,0.5);
+    [ Sens_beta, Spec_beta] = results_Sens_Spec_RRMSE_vetterli_two_way(B,B_tilde,x,theta,0.5);
     Sens_DistD(l,1)=mean(Sens_beta);
     Spec_DistD(l,1)=mean(Spec_beta);
     % generating the inverse W matrix
     W=weight_W(A);
-[~, ~, Sens_beta, Spec_beta, ~] = results_Sens_Spec_RRMSE_gauss(A_tilde, A, beta, delta, sigma, W);
+[~, ~, Sens_beta, Spec_beta, ~] = results_Sens_Spec_RRMSE_gauss(B_tilde, B, A_tilde, A, beta, delta, sigma, W);
 Sens_Odrlt(l,1)=mean(Sens_beta);
 Spec_Odrlt(l,1)=mean(Spec_beta);
+
 end
